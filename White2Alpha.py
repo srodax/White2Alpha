187f1416af616a1da0901f7d7dc266841938db7c
srodax <ms@maksymiliansroda.pl>
2022-03-29 16:20:04 +0200

    added function which transform rgb to rgba

--- White2Alpha.py
+++ White2Alpha.py
@@ -15,7 +15,58 @@ args = parser.parse_args()
 data = np.loadtxt(fileinput.input(args.files), dtype=float)
 #***********************************************************************
 
+# Taken from [https://stackoverflow.com/a/68809469], [https://colab.research.google.com/drive/1iJ-3ZdJ822JlZgJ3ZtGCyClzT5X7Wk0t#scrollTo=lc0hG1PKrbV3]
+def rgb_white2alpha(rgb, ensure_increasing=False, ensure_linear=False, lsq_linear=False):
+    """
+    Convert a set of RGB colors to RGBA with maximum transparency.
 
+    The transparency is maximised for each color individually, assuming
+    that the background is white.
+
+    Parameters
+    ----------
+    rgb : array_like shaped (N, 3)
+        Original colors.
+    ensure_increasing : bool, default=False
+        Ensure that alpha values increase monotonically.
+    ensure_linear : bool, default=False
+        Ensure alpha values increase linear from initial to final value.
+    lsq_linear : bool, default=False
+        Use least-squares linear fit for alpha.
+
+    Returns
+    -------
+    rgba : numpy.ndarray shaped (N, 4)
+        Colors with maximum possible transparency, assuming a white
+        background.
+    """
+    # The most transparent alpha we can use is given by the min of RGB
+    # Convert it from saturation to opacity
+    alpha = 1. - np.min(rgb, axis=1)
+    if lsq_linear:
+        # Make a least squares fit for alpha
+        indices = np.arange(len(alpha))
+        A = np.stack([indices, np.ones_like(indices)], axis=-1)
+        m, c = np.linalg.lstsq(A, alpha, rcond=None)[0]
+        # Use our least squares fit to generate a linear alpha
+        alpha = c + m * indices
+        alpha = np.clip(alpha, 0, 1)
+    elif ensure_linear:
+        # Use a linearly increasing/decreasing alpha from start to finish
+        alpha = np.linspace(alpha[0], alpha[-1], rgb.shape[0])
+    elif ensure_increasing:
+        # Let's also ensure the alpha value is monotonically increasing
+        a_max = alpha[0]
+        for i, a in enumerate(alpha):
+            alpha[i] = a_max = np.maximum(a, a_max)
+    alpha = np.expand_dims(alpha, -1)
+    # Rescale colors to discount the white that will show through from transparency
+    rgb = (rgb + alpha - 1)
+    rgb = np.divide(rgb, alpha, out=np.zeros_like(rgb), where=(alpha > 0))
+    rgb = np.clip(rgb, 0, 1)
+    # Concatenate our alpha channel
+    rgba = np.concatenate((rgb, alpha), axis=1)
+    return rgba
 
 # printing the result to std out
 #np.savetxt(sys.stdout, np.stack(datanew,axis=1), newline='\n')
